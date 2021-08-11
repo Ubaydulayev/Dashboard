9f0390230b471b3ed6497f5d70768b83722ee317
Ubaydulayev <ubaydullayevdastonbek@gmail.com>
2021-08-11 11:30:13 +0500

    dashboard

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Dashboards from './pages/Dashboards';
+import Home from './pages/Home';
+import store from './redux/store';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/dashboard/home" />
+          </Route>
+          <Route path="/dashboard/:page"><Dashboards /></Route>
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 
